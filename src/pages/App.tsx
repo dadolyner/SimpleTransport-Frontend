bcedfcdfc4f89d86b6a421feb8c27714b3c66113
dadolyner <skulj.david@gmail.com>
2022-08-23 15:28:12 +0200

    added seperate homepage

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,19 +1,20 @@
 import '../styles/GlobalStyles.css';
 import * as React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import CarsContainer from '../components/Cars/CarsContainer/CarsContainer';
 import Login from '../components/Forms/Login/Login';
 import Register from '../components/Forms/Register/Register';
 import Options from '../components/Forms/Options/Options';
 import ResetPassword from '../components/Forms/ForgotPassword/ResetPassword';
 import ForgotPassword from '../components/Forms/ForgotPassword/ForgotPassword';
-import CarsContainer from '../components/Cars/CarsContainer/CarsContainer';
 
 const App: React.FC = () => {
 	return (
 		<>
 			<BrowserRouter>
 				<Routes>
-					<Route path="/" element={<CarsContainer />} />
+					<Route path="/" element={<Home />} />
 					<Route path="/cars" element={<CarsContainer />} />
 					<Route path="/login" element={<Login />} />
 					<Route path="/register" element={<Register />} />
