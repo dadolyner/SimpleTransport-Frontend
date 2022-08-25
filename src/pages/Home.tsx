9c43db94498a5bb7e5f69e0008e795ffa566c74d
dadolyner <skulj.david@gmail.com>
2022-08-25 15:46:00 +0200

    developing homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,28 +1,13 @@
 import * as React from 'react';
-import Popup from '../components/Popups/popup';
+import Footer from '../components/Footer/footer';
+import Navigation from '../components/Navigation/navigation';
 
 const Home: React.FC = () => {
-    const [popupValues, setPopupValues] = React.useState({});
-    const [popupVisible, setPopupVisible] = React.useState(false);
-
     return (
         <>
-            <button onClick={() => (popupVisible ? setPopupVisible(false) : setPopupVisible(true))}>Open Popup</button>
-            { popupVisible && <Popup
-                key={'TestPopup'}
-                active={ popupVisible }
-                size={600}
-                title={'Test Popup'}
-                topClose={() => setPopupVisible(false)}
-                labelAligment={'center'}
-                theme={{ primary: '#fff', primaryDarken: '#000', secondary: '#000', secondaryDarken: '#000', text: '#000' }}
-                inputs={[{ type: 'text', label: 'Test Input', name: 'test_input', value: '' }]}
-                bottomButtons={[{ name: 'confirm', text: 'Test Button', onClick: () => {} }]}
-                RetrieveValues={(values) => {
-                    setPopupValues(values);
-                    setPopupVisible(false);
-                }}
-            />}
+            <Navigation />
+
+            <Footer />
         </>
     );
 };
