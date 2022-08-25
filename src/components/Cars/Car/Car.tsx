2bd642c62e7e15ca2e2e4c322c704e706f08e5e6
dadolyner <skulj.david@gmail.com>
2022-08-25 15:34:33 +0200

    setup rental system

--- src/components/Cars/Car/Car.tsx
+++ src/components/Cars/Car/Car.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
-import { Container, CarImageInfo, Image, CarSpecsInfo, CarPriceInfo, Header, Button, GridFullRow, CarSpecs, CarPrice, CarReturn, Icon, SingleRow } from '../cars.styled';
+import { Container, CarImageInfo, Image, CarSpecsInfo, CarPriceInfo, Header, Button, GridFullRow, CarSpecs, CarPrice, Icon, SingleRow } from '../cars.styled';
 import { Images } from './icons/icons';
 import Toast from '../../Toast/toast';
+import Popup from '../../Popups/popup';
+import axios from '../../../api/axios';
+import Seats from '../../../helpers/Seats'
+import Duration from '../../../helpers/Duration'
 
 type carInfo = {
 	key: string;
-	avalible: boolean;
+    id: string;
 	image: string;
 	name: string;
 	seats: number;
@@ -20,32 +24,69 @@ type carInfo = {
 };
 
 const Car: React.FC<carInfo> =  (props: carInfo) => {
-	const [toast, setToast] = React.useState(false);
+	const [toastSuccess, setToastSuccess] = React.useState(false);
+	const [toastSuccessMessage, setToastSuccessMessage] = React.useState('');
+	const [toastError, setToastError] = React.useState(false);
+	const [toastErrorMessage, setToastErrorMessage] = React.useState('');
+    const [popupVisible, setPopupVisible] = React.useState(false);
 
-	const { avalible, duration, image, name, seats, shifter, horsepower, torque, speed, location, price } = props;
-	let { fuel } = props;
-	let isFree = (price <= 1000) ? true : false;
+	const { id, image, name, seats, shifter, horsepower, torque, speed, fuel, location, price, duration } = props;
+    const RentACar = async(values: { rent_start: string, rent_end: string }) => {
+        try {       
+            const accessToken = localStorage.getItem('simpletransport_accessToken');
+            const rentalData = { rent_start: values.rent_start, rent_end: values.rent_end, vehicleId: id }
 
-	const allowedFuels = ["Dizel", "Bencin", "Plin", "Hibrid", "Elektrika"]
-	if(!allowedFuels.includes(fuel)) fuel = 'Na'
+            await axios.post('/rental', rentalData, { headers: { Authorization: `Bearer ${accessToken}` } }) 
 
-	const Duration = (days: number) => {
-		if (days < 1) return
-		else if(days === 1) return "Cena za 1 dan:";
-		else return `Cena za ${days} dni:`;
-	}
-
-	const Seats = (seats: number) => {
-		if (seats < 1) return
-		else if(seats === 1) return "1 Sedež";
-		else if (seats === 2) return "2 Sedeža";
-		else if (seats === 3 || seats === 4) return `${seats} Sedeži`;
-		else return `${seats} Sedežev`;
-	}
+            setToastSuccessMessage(`Car ${name} successfully rented!`);
+            setToastSuccess(true);
+            setTimeout(() => { setToastSuccess(false); }, 5100);
+            
+            setPopupVisible(false);
+        } 
+        catch(error) { 
+            if (error.response.status === 400) {
+                setToastErrorMessage('Please fill all fields!');
+                setToastError(true);
+            } else {
+                setToastErrorMessage(error.response.data.message);
+                setToastError(true);
+            }
+            setTimeout(() => { setToastError(false) }, 5100);
+        }
+        
+    }
 
 	return (
 		<>
-			{ toast && !avalible && <Toast type={'error'} message={'Izbran avtomobil trenutno ni na voljo!'} /> }
+			{ toastSuccess && <Toast type={'success'} message={toastSuccessMessage} /> }
+			{ toastError && <Toast type={'error'} message={toastErrorMessage} /> }
+            
+            { popupVisible && <Popup
+                key={name + 'RentPopup'}
+                active={ popupVisible }
+                title={'Rent ' + name}
+                size={600}
+                theme={{ background: '#fff', border: 'linear-gradient(240deg, #efb467 0%, #de8667 100%)', text: '#000' }}
+                labelAligment={'center'}
+                topClose={() => setPopupVisible(false)}
+                inputs={[
+                    { type: 'datetime-local', label: 'Rent start', name: 'rent_start' },
+                    { type: 'datetime-local', label: 'Rent end', name: 'rent_end' }
+                ]}
+                bottomButtons={[
+                    { 
+                        name: 'confirm', 
+                        text: 'Confirm', 
+                        color: '#fff', 
+                        colorHover:'#de8667', 
+                        background: 'linear-gradient(240deg, #efb467 0%, #de8667 100%)', 
+                        backgroundHover: '#fff', 
+                        onClick: () => {} 
+                    },
+                ]}
+                RetrieveValues={(values) => { RentACar(values) }}
+            />}
 
 			<Container>
 				<CarImageInfo>
@@ -54,20 +95,19 @@ const Car: React.FC<carInfo> =  (props: carInfo) => {
 
 				<CarSpecsInfo className='CarSpecsInfo'>
 					<GridFullRow><Header>{name}</Header></GridFullRow>
-					<SingleRow title="Število sedežev"><Icon src={Images.seats} alt="Seats"/><CarSpecs>{Seats(seats)}</CarSpecs></SingleRow>
-					<SingleRow title="Vrsta menjalnika"><Icon src={Images.shifter} alt="Shifter"/><CarSpecs>{shifter}</CarSpecs></SingleRow>
-					<SingleRow title="Število konjskih moči"><Icon src={Images.engine} alt="Engine"/><CarSpecs>{horsepower} Hp</CarSpecs></SingleRow>
-					<SingleRow title="Navor"><Icon src={Images.torque} alt="Torque"/><CarSpecs>{torque} Nm</CarSpecs></SingleRow>
-					<SingleRow title="Čas od 0 - 100 km/h"><Icon src={Images.speed} alt="Speed"/><CarSpecs>0-100km/h v {speed} sec</CarSpecs></SingleRow>
-					<SingleRow title="Vrsta goriva"><Icon src={Images.fuel} alt="Fuel"/><CarSpecs>{fuel}</CarSpecs></SingleRow>
-					<SingleRow title="Lokacija prevzema"><Icon src={Images.location} alt="Location" /><CarSpecs>{location}</CarSpecs></SingleRow>
+					<SingleRow title="Number of seats"><Icon src={Images.seats} alt="Seats"/><CarSpecs>{Seats(seats)}</CarSpecs></SingleRow>
+					<SingleRow title="Shifter type"><Icon src={Images.shifter} alt="Shifter"/><CarSpecs>{shifter}</CarSpecs></SingleRow>
+					<SingleRow title="Horsepower"><Icon src={Images.engine} alt="Engine"/><CarSpecs>{horsepower} Hp</CarSpecs></SingleRow>
+					<SingleRow title="Torque"><Icon src={Images.torque} alt="Torque"/><CarSpecs>{torque} Nm</CarSpecs></SingleRow>
+					<SingleRow title="Time from 0 - 100 km/h"><Icon src={Images.speed} alt="Speed"/><CarSpecs>{speed} sec</CarSpecs></SingleRow>
+					<SingleRow title="Fuel type"><Icon src={Images.fuel} alt="Fuel"/><CarSpecs>{fuel}</CarSpecs></SingleRow>
+					<SingleRow title="Pickup location"><Icon src={Images.location} alt="Location" /><CarSpecs>{location}</CarSpecs></SingleRow>
 				</CarSpecsInfo>
 
 				<CarPriceInfo className='CarPriceInfo'>
 					<div>{Duration(duration)}</div>
 					<CarPrice>{price} €</CarPrice>
-					<CarReturn className={isFree ? 'free' : 'paid'}>{isFree ? `Brezplačna odpoved` : `Odpoved plačljiva`}</CarReturn>
-					<Button onClick={() => {setToast(true); setTimeout(()=> {setToast(false)}, 5100)}}>Rent</Button>	
+					<Button onClick={() => { setPopupVisible(true) }}>Rent</Button>	
 				</CarPriceInfo>
 			</Container>
 		</>
--- src/components/Cars/CarsContainer/CarsContainer.tsx
+++ src/components/Cars/CarsContainer/CarsContainer.tsx
@@ -6,6 +6,7 @@ import Navigation from '../../Navigation/navigation'
 import Footer from '../../Footer/footer'
 import axios from '../../../api/axios'
 import { CarAvatar } from '../../../images/ImageExporter'
+import BackToTop from '../../BackToTop/backtotop'
 
 const CarsContainer: React.FC = () => {
     const [cars, setCars] = React.useState([])
@@ -33,7 +34,7 @@ const CarsContainer: React.FC = () => {
 						{cars.map((car) => {
 							return <Car 
 								key={car.vehicle.id}
-								avalible={true} 
+                                id={car.vehicle.id}
 								image={car.vehicle.image ? car.vehicle.image : CarAvatar} 
 								name={car.vehicle.brand + ' ' + car.vehicle.model}
 								seats={car.vehicle.seats} 
@@ -42,13 +43,15 @@ const CarsContainer: React.FC = () => {
 								torque={car.vehicle.torque} 
 								speed={car.vehicle.acceleration} 
 								fuel={car.vehicle.fuel} 
-								location={car.vehicle.country} 
+								location={car.user.place + ', ' + car.user.country} 
 								price={car.vehicle.price} 
 								duration={car.vehicle.rent_duration} 
 							/>;
 						})}
 					</CarsListContainer>
 				</BigParentContainer>
+
+                <BackToTop />
 
 				<Footer />
 			</Background>
