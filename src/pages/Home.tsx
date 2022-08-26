a6622acbccf3bf467fc7b68f8f7715951e9a5968
dadolyner <skulj.david@gmail.com>
2022-08-26 13:31:26 +0200

    added home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,16 @@
 import * as React from 'react';
+import { Background } from '../components/Cars/cars.styled';
 import Footer from '../components/Footer/footer';
 import Navigation from '../components/Navigation/navigation';
 
 const Home: React.FC = () => {
     return (
         <>
-            <Navigation />
+            <Background>
+                <Navigation />
 
-            <Footer />
+                <Footer />
+            </Background>
         </>
     );
 };
