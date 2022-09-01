5e291a680fcad8a56061cd96dc09db7310d0a3cf
dadolyner <skulj.david@gmail.com>
2022-09-01 10:41:06 +0200

    change server ip

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: `${process.env.REACT_APP_SERVER_IP}`
+    baseURL: `https://simple-transport-backend.herokuapp.com`
 })
 
 export default api;
